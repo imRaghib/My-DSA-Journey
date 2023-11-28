9ec090b669044b08b0b20c475726ad084093e2e0
Raghib <raghib108@gmail.com>
2023-11-28 18:58:13 +0500

    _

---
+++ Day 1/03-BigO.js
@@ -0,0 +1,12 @@
+const nemo = ["nemo"];
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found NEMO!");
+    }
+  }
+}
+findNemo(nemo);
+
+// 015 Big O Rule 2
