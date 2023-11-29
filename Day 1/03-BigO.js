018219ea4e072001c41651d0934002b91ba58f04
Raghib <raghib108@gmail.com>
2023-11-29 15:41:50 +0500

    _

--- Day 1/03-BigO.js
+++ Day 1/03-BigO.js
@@ -7,6 +7,15 @@ function findNemo(array) {
     }
   }
 }
-findNemo(nemo);
+// findNemo(nemo);
 
-// 015 Big O Rule 2
+// 017
+
+//// Log all paris of array
+const boxes = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < boxes.length; i++) {
+  for (let j = 0; j < boxes.length; j++) {
+    console.log([boxes[i], boxes[j]]);
+  }
+}
