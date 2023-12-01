b9ea4dfd0cc199c04ad7ac99f4bef26be3f35f51
Raghib <raghib108@gmail.com>
2023-12-01 17:52:36 +0500

    _

--- Day-4/07-Data-Structures-Hash-Tables.js
+++ Day-4/07-Data-Structures-Hash-Tables.js
@@ -9,6 +9,7 @@ class HashTable {
     }
     return hash;
   }
+
   set(key, value) {
     let address = this._hash(key);
     if (!this.data[address]) {
@@ -17,6 +18,7 @@ class HashTable {
     this.data[address].push([key, value]);
     return this.data;
   }
+
   get(key) {
     let address = this._hash(key);
     const currentBucket = this.data[address];
@@ -25,10 +27,23 @@ class HashTable {
       return currentBucket.find((bucket) => bucket[0] === key)[1];
     }
   }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
 }
 
-const myHashTable = new HashTable(2);
+const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
 myHashTable.set("apples", 5000);
 const getBucket = myHashTable.get("grapes");
 console.log(getBucket);
+
+const keys = myHashTable.keys();
+console.log(keys);
