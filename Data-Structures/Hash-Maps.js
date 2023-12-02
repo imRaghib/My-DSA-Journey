f80c158db03f21e60f2e47c4d4571b744fb0d3af
Raghib <raghib108@gmail.com>
2023-12-02 18:23:45 +0500

    _

---
+++ Data-Structures/Hash-Maps.js
@@ -0,0 +1,49 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+
+    if (currentBucket) {
+      return currentBucket.find((bucket) => bucket[0] === key)[1];
+    }
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 10000);
+myHashTable.set("apples", 5000);
+const getBucket = myHashTable.get("grapes");
+console.log(getBucket);
+
+const keys = myHashTable.keys();
+console.log(keys);
