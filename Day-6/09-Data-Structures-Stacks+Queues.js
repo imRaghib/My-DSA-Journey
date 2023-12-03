207e38a2201e572d732cf67244b7d58864f042c6
imRaghib <raghib108@gmail.com>
2023-12-03 15:48:56 +0500

    _

--- Day-6/09-Data-Structures-Stacks+Queues.js
+++ Day-6/09-Data-Structures-Stacks+Queues.js
@@ -84,3 +84,50 @@ myStack.pop();
 
 const response = myStack.peak();
 console.log(response);
+
+//Queues
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    if (this.length == 0) return "Queue is empty!";
+    return this.first;
+  }
+  enqueue(value) {
+    const newNode = Node(value);
+    if (this.length == 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+  isEmpty() {
+    if (this.length == 0) return "Queue is empty!";
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue(12);
+myQueue.enqueue(16);
+myQueue.enqueue(19);
