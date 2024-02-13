ef07cf8fe91b273229906356dab3d768dc6c6b89
Raghib <raghib108@gmail.com>
2024-02-13 22:21:00 +0500

    _

--- Day-9/12-Algorithms-Recursion.js
+++ Day-9/12-Algorithms-Recursion.js
@@ -1,7 +1,28 @@
+var answer = undefined;
 function findFactorialRecursive(number) {
-  return answer;
+  if (answer === undefined) {
+    answer = number;
+  }
+  if (number > 1) {
+    answer = answer * (number - 1);
+    number--;
+  } else {
+    return answer;
+  }
+
+  return findFactorialRecursive(number);
 }
 
 function findFactorialIterative(number) {
+  var answer = number;
+  for (let i = 0; i < number; i++) {
+    answer = answer * (number - 1);
+    number--;
+  }
   return answer;
 }
+res = findFactorialRecursive(5);
+console.log(res);
+
+res2 = findFactorialIterative(5);
+console.log(res2);
