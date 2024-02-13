04714267e604c4c5002bfb819faa40ecec4ebb8f
Raghib <raghib108@gmail.com>
2024-02-13 22:56:53 +0500

    _

--- Day-9/12-Algorithms-Recursion.js
+++ Day-9/12-Algorithms-Recursion.js
@@ -21,8 +21,63 @@ function findFactorialIterative(number) {
   }
   return answer;
 }
+// res = findFactorialRecursive(5);
+// console.log(res);
+
+// res2 = findFactorialIterative(5);
+// console.log(res2);
+
+//His solution
+
+/*
+function findFactorialRecursive(number) {
+  if (number === 2) {
+    return 2;
+  }
+
+  return number * findFactorialRecursive(number - 1);
+}
+
+function findFactorialIterative(number) {
+  var answer = 1;
+  if (number === 2) {
+    return 2;
+  }
+  for (let i = 2; i <= number; i++) {
+    answer = answer * i;
+  }
+  return answer;
+}
 res = findFactorialRecursive(5);
 console.log(res);
 
 res2 = findFactorialIterative(5);
 console.log(res2);
+
+*/
+
+// given index return fibonacci number
+function fibonacciIerative(n) {
+  let arr = [0, 1];
+  if (index < 2) {
+    return index;
+  }
+
+  for (let i = 2; i < n - 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+  return arr[n];
+}
+
+function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+res = fibonacciIerative(8);
+console.log(res);
+
+// res2 = fibonacciRecursive(5);
+// console.log(res2);
